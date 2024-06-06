784f0fe4344da6f5b7cc6c29563d0dea2266224b
AdaoPedro <adaopedro.pro@gmail.com>
2024-06-06 22:24:08 +0100

    feat: add the app and the app http server using reactphp

---
+++ server.php
@@ -0,0 +1,34 @@
+<?php
+
+    use React\Http\HttpServer;
+    use Psr\Http\Message\ServerRequestInterface as Request;
+    use React\Http\Message\Response;
+    use React\Socket\SocketServer;
+    use \Exception;
+
+    require __DIR__ . "/vendor/autoload.php";
+
+    $httpServer = new HttpServer(
+        function(Request $request) {
+            return Response::plaintext("Ola mundo!!!\n");
+        }
+    );
+
+    $httpServer->on("error", function (Exception $ex) {
+        echo "Error: " . $ex->getMessage();
+
+        $previousException = $ex->getPrevious();
+
+        echo $previousException !== null
+            ? "Previous error: " . $previousException->getMessage() . PHP_EOL
+            : "";
+    });
+
+
+    $socketServer =  new SocketServer("0.0.0.0:80");
+    $httpServer->listen($socketServer);
+
+    echo "Server running on "
+        . str_replace("tcp", "http", $socketServer->getLocalAddress())
+        . PHP_EOL;
+
