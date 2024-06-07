8998feed5e02d9af66bb7f9b477e8d3247b4e124
AdaoPedro <adaopedro.pro@gmail.com>
2024-06-07 03:32:51 +0100

    feat: refactor the request routing implementing OOP approach instead of functional one

--- public/server.php
+++ public/server.php
@@ -1,58 +1,34 @@
 <?php
 
     use React\Http\HttpServer;
-    use Psr\Http\Message\ServerRequestInterface as Request;
-    use React\Http\Message\Response;
     use React\Socket\SocketServer;
 
-    use FastRoute\ConfigureRoutes;
-    use FastRoute\Dispatcher;
-    use function FastRoute\simpleDispatcher;
+    use React\Http\Message\Response;
+    use Psr\Http\Message\ServerRequestInterface;
+
+    use App\Core\Router;
+    use FastRoute\DataGenerator\GroupCountBased;
+    use FastRoute\RouteCollector;
+    use FastRoute\RouteParser\Std;
 
     require dirname(__DIR__) . DIRECTORY_SEPARATOR .  "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
 
-    $dispatcher = simpleDispatcher( function ($routes) {
-        $routes->get("/", function(Request $request) {
-            return Response::plaintext("GET /");
-        });
+    $routes = new RouteCollector(new Std(), new GroupCountBased());
+    
+    $routes->get("/", function(ServerRequestInterface $request) {
+        return Response::plaintext("GET /");
+    });
 
-        $routes->get("/products", function (Request $request) {
-            return Response::plaintext("GET /products");
-        });
-       
-        $routes->post("/products", function (Request $request) {
-            return Response::plaintext("POST /products");
-        });
-    } );
-
+    $routes->get("/products", function (ServerRequestInterface $request) {
+        return Response::plaintext("GET /products");
+    });
+   
+    $routes->post("/products", function (ServerRequestInterface $request) {
+        return Response::plaintext("POST /products");
+    });
 
     $httpServer = new HttpServer(
-        function(Request $request) use ($dispatcher) {
-            $httpMethod = $request->getMethod();
-            $uri = $request->getUri()->getPath();
-
-            $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
-            switch($routeInfo[0]) {
-                case Dispatcher::NOT_FOUND:
-                    // ... 404 Not found
-                    return Response::plaintext("404 Not found");
-                    break;
-                case Dispatcher::METHOD_NOT_ALLOWED:
-                    $allowedMethods = $routeInfo[1];
-                    return Response::plaintext("405 Method Not Allowed");
-                    // ... 405 Method Not Allowed
-                    break;
-                case Dispatcher::FOUND:
-                    $handler = $routeInfo[1];
-                    $vars = $routeInfo[2];
-                    // ... call $handler with $vars
-                    return $handler($request, ...$vars);
-                    break;
-                    
-                
-            }
-            throw new \LogicException("Error on routing");
-        }
+        new Router($routes)
     );
 
     $httpServer->on("error", function (\Exception $ex) {
---
+++ src/Core/Router.php
@@ -0,0 +1,39 @@
+<?php
+    declare(strict_types=1);
+
+    namespace App\Core;
+
+    use LogicException;
+    
+    use FastRoute\Dispatcher;
+    use FastRoute\RouteCollector;
+    use FastRoute\Dispatcher\GroupCountBased;
+
+    use Psr\Http\Message\ServerRequestInterface;
+    use React\Http\Message\Response;
+
+    final class Router {
+        private Dispatcher $dispatcher;
+
+        public function __construct (RouteCollector $routes) {
+            $this->dispatcher = new GroupCountBased($routes->getData());
+        }
+
+        public function __invoke (ServerRequestInterface $request) {
+            $routeInfo = $this->dispatcher->dispatch(
+                $request->getMethod(), $request->getUri()->getPath()
+            );
+    
+            switch ($routeInfo[0]) {
+                case Dispatcher::NOT_FOUND:
+                    return new Response(404, ['Content-Type' => 'text/plain'], 'Not found');
+                case Dispatcher::METHOD_NOT_ALLOWED:
+                    return new Response(405, ['Content-Type' => 'text/plain'], 'Method not allowed');
+                case Dispatcher::FOUND:
+                    $handler = $routeInfo[1];
+                    return $handler($request, ...array_values($routeInfo[2]));
+            }
+
+            throw new LogicException('Something went wrong with routing');
+        }
+    }
